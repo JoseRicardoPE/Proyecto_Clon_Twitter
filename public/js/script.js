a98f2fce6f44c66304387859b6e93661ce5797d9
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-05-30 17:39:45 -0300

    Función de botón flechita en registro

--- public/js/script.js
+++ public/js/script.js
@@ -7,17 +7,29 @@ const button1 = document.getElementById("button-1");
 const button2 = document.getElementById("button-2");
 const button3 = document.getElementById("button-3");
 
-button1.addEventListener("click", function () {
+const arrow1 = document.getElementById("arrow-1");
+const arrow2 = document.getElementById("arrow-2");
+const arrow3 = document.getElementById("arrow-3");
+
+function contenedoresUnoYDos() {
   contenedor1.classList.toggle("ocultar");
   contenedor2.classList.toggle("ocultar");
-});
+}
 
-button2.addEventListener("click", function () {
+function contenedoresDosYTres() {
   contenedor2.classList.toggle("ocultar");
   contenedor3.classList.toggle("ocultar");
-});
+}
 
-button3.addEventListener("click", function () {
+function contenedoresTresYCuatro() {
   contenedor3.classList.toggle("ocultar");
   contenedor4.classList.toggle("ocultar");
-});
+}
+button1.addEventListener("click", contenedoresUnoYDos);
+arrow1.addEventListener("click", contenedoresUnoYDos);
+
+button2.addEventListener("click", contenedoresDosYTres);
+arrow2.addEventListener("click", contenedoresDosYTres);
+
+button3.addEventListener("click", contenedoresTresYCuatro);
+arrow3.addEventListener("click", contenedoresTresYCuatro);
